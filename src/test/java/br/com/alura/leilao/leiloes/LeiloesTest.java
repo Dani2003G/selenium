a9cfa840603ca613742d9df67d02ebf95c640a6e
Daniel Gonçalves De Almeida <daniel0202almeida@outlook.com>
2025-05-21 22:46:49 -0300

    Adicao do testes, validando se o leilao foi cadastrado

---
+++ src/test/java/br/com/alura/leilao/leiloes/LeiloesTest.java
@@ -0,0 +1,35 @@
+package br.com.alura.leilao.leiloes;
+
+import br.com.alura.leilao.login.LoginPage;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+public class LeiloesTest {
+
+    private LeiloesPage paginaDeLeiloes;
+
+    @AfterEach
+    public void afterEach() {
+        this.paginaDeLeiloes.fechar();
+    }
+
+    @Test
+    public void deveriaCadastrarLeilao() {
+        LoginPage paginaDeLogin = new LoginPage();
+        paginaDeLogin.preencherFormularioLogin("fulano", "pass");
+        this.paginaDeLeiloes = paginaDeLogin.efetuarLogin();
+        CadastroLeilaoPage paginaDeCadastro = paginaDeLeiloes.carregarFormulario();
+
+        String hoje = LocalDate.now().format(DateTimeFormatter.ofPattern("dd/MM/yyyy"));
+        String nome = "Leilao do dia " + hoje;
+        String valor = "500.00";
+
+        this.paginaDeLeiloes = paginaDeCadastro.cadastrarLeilao(nome, valor, hoje);
+        Assertions.assertTrue(paginaDeLeiloes.isLeilaoCadastrado(nome, valor, hoje));
+    }
+
+}
