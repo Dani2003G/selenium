595d4bba9327cf52697e16136b2567d82775b1b4
Daniel Gonçalves De Almeida <daniel0202almeida@outlook.com>
2025-05-22 21:19:48 -0300

    Adicao do teste para validar o cadastro de leilao e tratativa para não repetir metodos

--- src/test/java/br/com/alura/leilao/leiloes/LeiloesTest.java
+++ src/test/java/br/com/alura/leilao/leiloes/LeiloesTest.java
@@ -3,6 +3,7 @@ package br.com.alura.leilao.leiloes;
 import br.com.alura.leilao.login.LoginPage;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDate;
@@ -12,6 +13,16 @@ public class LeiloesTest {
 
     private LeiloesPage paginaDeLeiloes;
 
+    private CadastroLeilaoPage paginaDeCadastro;
+
+    @BeforeEach
+    public void beforeEach() {
+        LoginPage paginaDeLogin = new LoginPage();
+        paginaDeLogin.preencherFormularioLogin("fulano", "pass");
+        this.paginaDeLeiloes = paginaDeLogin.efetuarLogin();
+        this.paginaDeCadastro = paginaDeLeiloes.carregarFormulario();
+    }
+
     @AfterEach
     public void afterEach() {
         this.paginaDeLeiloes.fechar();
@@ -19,17 +30,20 @@ public class LeiloesTest {
 
     @Test
     public void deveriaCadastrarLeilao() {
-        LoginPage paginaDeLogin = new LoginPage();
-        paginaDeLogin.preencherFormularioLogin("fulano", "pass");
-        this.paginaDeLeiloes = paginaDeLogin.efetuarLogin();
-        CadastroLeilaoPage paginaDeCadastro = paginaDeLeiloes.carregarFormulario();
-
         String hoje = LocalDate.now().format(DateTimeFormatter.ofPattern("dd/MM/yyyy"));
         String nome = "Leilao do dia " + hoje;
         String valor = "500.00";
 
         this.paginaDeLeiloes = paginaDeCadastro.cadastrarLeilao(nome, valor, hoje);
         Assertions.assertTrue(paginaDeLeiloes.isLeilaoCadastrado(nome, valor, hoje));
+    }
+
+    @Test
+    public void deveriaValidarCadastroDeLeilao() {
+        this.paginaDeLeiloes = paginaDeCadastro.cadastrarLeilao("", "", "");
+        Assertions.assertFalse(this.paginaDeCadastro.isPaginaAtual());
+        Assertions.assertTrue(this.paginaDeLeiloes.isPaginaAtual());
+        Assertions.assertTrue(this.paginaDeCadastro.isMensagensDeValidacaoVisiveis());
     }
 
 }
